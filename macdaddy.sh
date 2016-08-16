dbd4252fa18a6ed6869a5edcb3d9924c526ffe7b
Adrian Buford <adrianbuford@gmail.com>
2016-08-15 20:41:51 -0500

    disc
    Changes mac of wireless interfaces then changes orig country to allow for 30dBm.

--- macdaddy.sh
+++ macdaddy.sh
@@ -4,7 +4,6 @@ ifconfig wlan1 down && ifconfig wlan0 down
 echo "Spoofing mac to known ALLOWED mac on wifi AP"
 macchanger --mac 11:22:33:44:55:67 wlan1 && macchanger --mac 11:22:33:44:55:66 wlan0
 echo "Bringing up those faces I told u about"
-
 ## a lil something extra i guess for kali / parrotos
 iw reg set GY
 #
